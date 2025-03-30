37dff59b1ecce43617b6c58539214e013b97dd26
omar369 <34948096+omar369@users.noreply.github.com>
2025-03-30 14:43:24 -0600

    quitar el inline de archivos grandes: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+	build: {
+    assetsInlineLimit: 0, // <- evita inline de archivos grandes, siempre los guarda separados
+    chunkSizeWarningLimit: 3000, // aumenta límite para warnings de tamaño
+  },
 })
